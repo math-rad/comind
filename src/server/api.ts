61364f09e4956cdb4ae83676bf124204f1b832f5
math.rad <ben.sir545@outlook.com>
2025-12-06 12:20:41 -0600

    asdf

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,44 +1,48 @@
 import { v7 } from "uuid"
 import { readFile, writeFile } from "fs/promises"
 import fixedIds from "./fixed-ids.json" with {type: "json"}
+import { types } from "util"
 
 export class API {
     public static nodes: Record<string, InstanceType<typeof API.Node>> = {}
+
     public static fetchNode(NodeId: string): InstanceType<typeof API.Node> {
         return API.nodes[NodeId]
     }
 
+    private static getNode(node: InstanceType<typeof API.Node> | string) {
+        return (typeof(node) === "string"? API.fetchNode(node) : node)
+    }
+
     public static Node = class {
         public ID: string
-        public type?: string
+        public label?: string
         public modified?: string
-        public edges: Record<number | string, InstanceType<typeof API.Node> | string>
+        public edges: Record<string, InstanceType<typeof API.Node> | string>
 
-        constructor(ID?: string) {
+        constructor(label?: string, edges: {}, ID?: string) {
             this.ID = ID || v7() // should I consider v7 parameters?
+            this.label = label
             this.edges = {}
-
             API.nodes[this.ID] = this
+
         }
 
         public attach(node: InstanceType<typeof API.Node>, label: string) {
             this.edges[label] = node
         }
     }
-
     public static Container = class extends API.Node {
         public members: (InstanceType<typeof API.Node> | string)[]
-        public label: string
+        public type = fixedIds.types.container
         constructor(label: string, members?: Array<InstanceType<typeof API.Node> | string>, ID?: string) {
-            super(ID)
-            this.members = members || []
-            this.label = label
-
+            super(label, ID)
+            this.members = []
             if (members) {
                 for (var member of members) {
                     switch (typeof (member)) {
                         case "string": {
-                            this.addMember(API.nodes[member])
+                            this.addMember(API.fetchNode(member))
                             break
                         };
                         default: {
@@ -47,35 +51,46 @@ export class API {
                     }
                 }
             }
+
+            
         }
 
         addMember(node: InstanceType<typeof API.Node> | string) {
-            this.members.push(node);
-            (typeof(node) == "string"? API.nodes[node] : node).attach(this, this.label)
+            node = API.getNode(node)
+            this.members.push(node)
+            node.attach(this, this.label)
             
         }
     }
 
-    public static Enum = class Enum extends API.Container {
-        type = fixedIds.types.enum
+    public static Enum = class extends API.Container {
+        public edges = {
+            type: fixedIds.types.type
+        }
     }
 
     public static Type = class Type extends API.Enum {
-        type = fixedIds.types.type
-
+        public edges = {
+            type: fixedIds.types.type
+        }
     }
 
     public static Tag = class Tag extends API.Enum {
-        public type = fixedIds.types.tag
-        
+        public edges = {
+            type: fixedIds.types.tag
+        }
+    }
+
+    public static indexers = {
+        //tag = (node: InstanceType<typeof API.Node> | string) => API.getNode(node).edges
     }
 
     constructor() {
-        new API.Container("types", [
-            new API.Type("container", [], fixedIds.types.container),
-            new API.Type("enum", [], fixedIds.types.enum),
-            new API.Type("tag", [], fixedIds.types.tag),
-            new API.Type("module", [], fixedIds.types.module),
-        ], "types")
+        const types = new API.Container("types", [
+            new API.Type("container", [], fixedIds.types.container).ID,
+            new API.Type("enum", [], fixedIds.types.enum).ID,
+            new API.Type("tag", [], fixedIds.types.tag).ID,
+            new API.Type("module", [], fixedIds.types.module).ID,
+        ])
     }
 }
